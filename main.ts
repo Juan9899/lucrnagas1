0a24f2addc80bb039e4e0d1f208e1749473281a8
Juan9899 <98743364+Juan9899@users.noreply.github.com>
2022-06-06 11:10:21 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,6 @@
 input.onButtonPressed(Button.A, function () {
     radio.sendString("1")
 })
-function flash () {
-	
-}
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "1") {
         basic.pause(randint(50, 350))
@@ -15,14 +12,14 @@ radio.onReceivedString(function (receivedString) {
             # # # # #
             `)
         for (let index = 0; index < 10; index++) {
-            Variable += -10
+            Variable += -5
             led.setBrightness(Variable)
             basic.pause(40)
         }
         basic.clearScreen()
         Variable = 255
         led.setBrightness(255)
-        if (randint(Variable, 9) == 0) {
+        if (randint(0, 100) <= 10) {
             basic.pause(500)
             radio.sendString("1")
         }
